86dacaa4952c7b889f7892bb1cbd9ad53947fd08
Jinal <Jinal@JSN>
2016-06-01 19:56:51 -0400

    added navbar usercontrol

---
+++ portfolioSite/User_Controls/navbar.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/**
+ * @author: Jinalkumar Patel
+ * @date: June 1, 2016
+ * @virsion 0.0.1
+*/
+namespace portfolioSite
+{
+    public partial class navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
